e54f76c57ca73d47abd6286b305ec9ec65591f17
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2022-10-18 15:16:40 +0200

    bug/hash: change the alphabet

--- src/lib/hash.ml
+++ src/lib/hash.ml
@@ -1,5 +1,9 @@
+let alphabet =
+  Base64.make_alphabet
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+_"
+
 let make content =
   let content = Cstruct.of_string content in
   Mirage_crypto.Hash.MD5.digest content
   |> Cstruct.to_string
-  |> Base64.encode_string ~pad:false ~alphabet:Base64.uri_safe_alphabet
+  |> Base64.encode_string ~pad:false ~alphabet
