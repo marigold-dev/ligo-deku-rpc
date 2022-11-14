596ed7fc749f31b698d2351135d4cd57465bb1f6
aguillon <aguillon@users.noreply.github.com>
2022-11-14 15:09:37 +0100

    Expressions helper (#5)
    * what postman
    
    * wip: Ligo expressions to WASM to help with CLI "invoke" command
    
    * I have been lied to, none of this is WASM.
    
    * fix: make sure the ligo file exists
    
    Co-authored-by: aguillon <arthur@marigold.dev>

--- src/lib/router.ml
+++ src/lib/router.ml
@@ -1,6 +1,8 @@
 open Piaf
 
-let ligo_to_tz ~env ~lang ~filename_ligo ~filename_tz () =
+(* TODO: factorize a bit if we keep this *)
+
+let compile_contract ~env ~lang ~filename_ligo ~filename_tz () =
   Eio.Switch.run @@ fun sw ->
   Logs.info (fun m -> m "compiling %s with syntax %s" filename_ligo lang);
   let stdout =
@@ -18,7 +20,15 @@ let ligo_to_tz ~env ~lang ~filename_ligo ~filename_tz () =
   in
   Eio.Flow.copy source sink
 
-let to_wasm ~env () =
+let compile_parameter ~lang ~filename_ligo ~expression () =
+  Logs.info (fun m -> m "compiling %s with syntax %s" filename_ligo lang);
+  let stdout =
+    Unix.open_process_args_in "ligo"
+      [| "ligo"; "compile"; "parameter"; filename_ligo; expression |]
+  in
+  In_channel.input_all stdout
+
+let source_to_tz ~env () =
   let handler { Server.ctx = _; request } =
     let json =
       request.body |> Body.to_string |> Result.map Yojson.Safe.from_string
@@ -52,7 +62,7 @@ let to_wasm ~env () =
             try Eio.Path.save ~create:(`Exclusive 0o600) ligo_path source
             with _ -> ()
           in
-          let () = ligo_to_tz ~env ~lang ~filename_ligo ~filename_tz () in
+          let () = compile_contract ~env ~lang ~filename_ligo ~filename_tz () in
           let tz = Eio.Path.load tz_path in
           tz
       | true ->
@@ -73,8 +83,58 @@ let to_wasm ~env () =
   in
   Routes.((s "api" / s "v1" / s "ligo" / s "originate" /? nil) @--> handler)
 
+let expression_to_tz ~env () =
+  let handler { Server.ctx = _; request } =
+    let json =
+      request.body |> Body.to_string |> Result.map Yojson.Safe.from_string
+    in
+    let source =
+      Result.map
+        (fun json -> Yojson.Safe.Util.(member "source" json |> to_string))
+        json
+      |> Result.get_ok
+    in
+    let lang =
+      Result.map
+        (fun json -> Yojson.Safe.Util.(member "lang" json |> to_string))
+        json
+      |> Result.get_ok
+    in
+    let expression =
+      Result.map
+        (fun json -> Yojson.Safe.Util.(member "expression" json |> to_string))
+        json
+      |> Result.get_ok
+    in
+
+    let hash = Hash.make source in
+    let filename_ligo = Printf.sprintf "%s.%s" hash lang in
+    let ligo_path = Eio.Path.(Eio.Stdenv.cwd env / filename_ligo) in
+    let ligo_already_exists =
+      try Some (Eio.Path.load ligo_path) |> Option.is_some with _ -> false
+    in
+    if not (ligo_already_exists) then begin
+      try Eio.Path.save ~create:(`Exclusive 0o600) ligo_path source
+      with _ -> ()
+    end;
+
+    let s = compile_parameter ~lang ~filename_ligo ~expression () in
+
+    let body =
+      `Assoc [ ("expression", `String s) ]
+      |> Yojson.Safe.to_string |> Piaf.Body.of_string |> Result.ok
+    in
+    match body with
+    | Ok body -> Piaf.Response.create ~body `OK
+    | Error e ->
+        Piaf.Response.create
+          ~body:(Body.of_string @@ Error.to_string e)
+          `Bad_request
+  in
+  Routes.((s "api" / s "v1" / s "ligo" / s "expression" /? nil) @--> handler)
+
 let healthz () =
   let handler _ = Piaf.Response.create ~body:(Piaf.Body.of_string "ok") `OK in
   Routes.((s "health" /? nil) @--> handler)
 
-let router ~env () = Routes.one_of [ to_wasm ~env (); healthz () ]
+let router ~env () = Routes.one_of [ source_to_tz ~env (); healthz () ; expression_to_tz ~env () ]
